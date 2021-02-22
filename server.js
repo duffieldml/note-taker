07728fbba0d31f201e7bc4692588a8dbe33da964
Matt Duffield <duffieldml@gmail.com>
2021-02-22 08:11:34 -0500

    Laying out server

--- server.js
+++ server.js
@@ -3,3 +3,17 @@ const express = require('express');
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(express.static('public'));
+
+//This will be the path for api routes
+require('./')(app);
+
+//this will be the path for html routes
+require('./')(app);
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port: ${PORT}`);
+})
