f81a541b360ca1ce62eb4dbb4a5ec6ca39d7c23d
Matt Duffield <duffieldml@gmail.com>
2021-02-22 08:37:27 -0500

    Working through api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,22 @@
 const fs = require('fs');
 const notes = require('../db/db.json');
 
+module.exports = (app) => {
+    app.get('/api/notes', (req, res) => {
+        return res.json(notes);
+    });
+
+    app.get('/api/notes/:id', (req, res) => {
+        const id = req.params.id;
+        let found;
+        notes.forEach(n => {
+            if (id == n.id){
+                found = n;
+                return res.json(n)
+            };
+        });
+        return res.json(false);
+    });
+
+    
+};
